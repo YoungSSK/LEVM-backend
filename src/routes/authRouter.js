4aba044dda51527bdf036522baf5611af8dc3e1e
YoungSSK <youngssk217@gmail.com>
2026-05-18 15:34:49 +0700

    Add refresh token functionality and fix response status in logout

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { Login, Logout, Register } from "../controllers/authController.js";
+import {
+  Login,
+  Logout,
+  refreshToken,
+  Register,
+} from "../controllers/authController.js";
 import { validate } from "../middlewares/validateMiddleware.js";
 import {
   loginUserSchema,
@@ -9,4 +14,5 @@ const router = express.Router();
 router.post("/register", validate(registerUserSchema), Register);
 router.post("/login", validate(loginUserSchema), Login);
 router.post("/logout", Logout);
+router.post("/refresh", refreshToken);
 export default router;
